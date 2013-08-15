c6981bc5f368683dd2db56fa7393f41d44819018
Spencer Claxton <s.a.claxton@gmail.com>
2013-08-15 01:05:59 -0500

    refactored status.js and added some final stylistic touches

--- dataviz/status.js
+++ dataviz/status.js
@@ -24,12 +24,35 @@ $j( document ).ready(function () {
         callback()
         setInterval(callback, 30000) 
     })()
-        
+
+    ////////////////////////////////////////////////////////// 
+    // GLOBAL VARIABLES AND OBJECTS //////////////////////////
+    //////////////////////////////////////////////////////////
+    
     var keys = Object.keys(JSON) 
-    var times = [] 
+    var times = []
+    // piechart dimensions and properties
+    var w = 260;
+    var h = 260;
+    var r = 120;
+    var ir = 0;
+    var textOffset = 18;
+    var tweenDuration = 250;
+    var valueLabels
+    var pieData = [];    
+    var oldPieData = [];
+    var filteredPieData = [];
+    var arc_group
+    var label_group
+ 
     $j.each(JSON, function (k, v) {
         times.push(v["time"])
-    });        
+    }); 
+
+    //////////////////////////////////////////////////////////
+    // XML CONTENT ///////////////////////////////////////////
+    //////////////////////////////////////////////////////////
+
     var chart = d3.select("body div")
         .attr("class", "chart")
     
@@ -49,7 +72,7 @@ $j( document ).ready(function () {
         .attr("class", "expand-bracket") 
         .text("[")
 
-    var expand_toggle = expand_span.append("a")
+    var expand_toggle = expand_span.append("span")
         .attr("class", "expander")
         .attr("data-expander-target", ".closed")
         .text("+")
@@ -65,47 +88,10 @@ $j( document ).ready(function () {
         .data(keys) 
         .enter().append("li")
         .attr("class", "header_container")
-    
-    // SVG IMPLEMENTATION
-
-    // var title_containers = header_containers.append("svg")
-    //     .attr("class", "title_container")
-    //     .attr("viewbox", "0 0 700 30")
-    
-    // var header_titles = title_containers.append("text") 
-    //     .attr("text-anchor", "start")
-    //     .attr("dx", 50)
-    //     .attr("class", "expander")
-    //     .text(function(d) { return d })
-    
-    // var text_height
-    
-    // header_titles.attr("y", function () {
-    //     var that = this
-    //     text_height = that.getBBox().height
-    //     return text_height
-    // })  
-    
-    // title_containers.attr("height", 2 * text_height ); 
-       
-    // var lines = title_containers.append("line")
-    //     .attr("y1", text_height + 10)
-    //     .attr("y2", text_height + 10)
-    //     .attr("x1", 50) 
-    //     .attr("x2", 700) 
-    //     .attr("stroke", "black")
-    //     .attr("stroke-width", 0.5)
-    //     .attr("stroke-opacity", 0.5)
-    //     .attr("shape-rendering", "crispEdges")
-
-    // HTML IMPLEMENTATION
- 
+     
     var title_containers = header_containers.append("div")
         .attr("class", "title_container")
-
-    // var header_titles_container = title_containers.append("div")
-    //     .attr("class", "header_titles_container")
-        
+            
     var header_titles = title_containers.append("span") 
         .attr("class", "expander title")
         .text(function(d) { return d })
@@ -128,105 +114,67 @@ $j( document ).ready(function () {
 
     $j(".expander").simpleexpand();
     
+    // expand all bracket animation
     var $closingBracket,$expandedOnly,$hiddenBracket,$outerClosingBracket,expandTimeout
-    var shrinkTimeout,$expand,$heading,$link,$link,$brackets
+    var shrinkTimeout,$expand,$heading,$link,$brackets
     $expand=$j('#expand')
     $heading=$j('#expand_heading')
-    $link=$expand.find('a').eq(0)
+    $link=$expand.find(".expander").eq(0)
     $link.attr("href","#expand")
     $brackets=$j([$expand.get(0).firstChild,$expand.get(0).lastChild]);
-    function toggleAll() {
-        $link.text(function() {
-            var $expanders = $j(".expander.title") 
-            var $expanded = $expanders.filter(".expanded")
-            var $collapsed = $expanders.filter(".collapsed")
-            var toggle = ""
-            if ($collapsed.length)  {
-                $link.attr("data-expander-target", "")
-                $collapsed.removeClass("collapsed").addClass("expanded")
-                toggle = "−" 
-            }
-            else {
-                $link.attr("data-expander-target", ".closed") 
-                $expanded.removeClass("expanded").addClass("collapsed")        
-                toggle = "+"
-            }
-            
-            $j(".expander.title").each( function (i, el) { 
-                setTimeout( function () { update_data(el) }, 200) 
-            });     
-            return toggle   
-        });
-    }
-    function expandSoon() {
-        clearTimeout(shrinkTimeout);
-        expandTimeout=setTimeout(expand,100);
-    }
-    function expand() {
-        clearTimeout(shrinkTimeout);
-        $heading.addClass('expand-expanded');
-        $link.attr("style","opacity:0.6;");
-    }
-    function shrinkSoon() {
-        clearTimeout(expandTimeout);
-        shrinkTimeout=setTimeout(shrink,100);
-    }
-    function shrink(){
-        clearTimeout(expandTimeout);
-        if(!$link.is(':focus') || !$link.hasClass('hover')){
-            $heading.removeClass('expand-expanded');
-            $link.attr("style", "opacity:1")
-        }        
-    }
+
     if(!$brackets.hasClass('expand-bracket')){
         $brackets=$brackets.wrap($j('<span>').addClass('expand-bracket')).parent();
     }
+
     $closingBracket=$brackets.last();
+
     $heading.on({'mouseenter':expandSoon,'mouseleave':shrinkSoon});
+
     $link.mouseenter(function(){
         $j(this).addClass('hover');
     }).mouseleave(function(){
         $j(this).removeClass('hover');
     });
+
     $link.on({'focus':expand, 'blur':shrinkSoon,'click':toggleAll});
 
-    ///////////////////////////////////////////////////////////////////////////////
-    //// PIECHART CODE MODIFIED FROM D3 TUTORIAL //////////////////////////////////
-    //// BY STEPHEN BOAK //////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////////
-        
-    var w = 300;
-    var h = 300;
-    var r = 110;
-    var ir = 0;
-    var textOffset = 18;
-    var tweenDuration = 250;
-        
-    //OBJECTS TO BE POPULATED WITH DATA LATER
-    var valueLabels
-    var pieData = [];    
-    var oldPieData = [];
-    var filteredPieData = [];
-    var arc_group
-    var label_group
-   
-    //D3 helper function to draw arcs, populates parameter "d" in path
-    //object
+    // initalize pie charts
     var arc = d3.svg.arc()
         .startAngle(function(d){ return d.startAngle; })
         .endAngle(function(d){ return d.endAngle; })
         .innerRadius(ir)
         .outerRadius(r);
-        
-    ///////////////////////////////////////////////////////////
-    // CREATE VIS & GROUPS ////////////////////////////////////
-    ///////////////////////////////////////////////////////////
     
     var h_list_containers = content_containers.append("div")
         .attr("class", "h_list_container")
 
     var h_list = h_list_containers.append("ul")
         .attr("class", "h_list")
+
+    var cloud_data = [] 
+    
+    for ( var cloud in JSON ) {
+        cloud_data.push(Object.keys(JSON[cloud]["stats"]))
+    }
+
+    var menu_items = h_list.each( function (d, i) {
+        var data = cloud_data[i]        
+        d3.select(this).selectAll("li")
+            .data(data)            
+            .enter().append("li")
+            .text(String)
+    });
+    
+    $j(".h_list li").hover( function (event) { $j(event.target).css("opacity", 0.5) }, 
+                            function (event) { var that = event.target;  
+                                 if (!$j(that).hasClass("clicked")) { 
+                                     $j(that).css("opacity", 1)
+                                 } } ).on("click", function (event) {
+                                     var that = event.target        
+                                     $j(that).closest(".h_list").contents().css("opacity", 1).removeClass("clicked")
+                                     $j(that).css("opacity", 0.5).addClass("clicked")
+                                 })                                             
 
     var vis_containers = content_containers.append("div")
         .attr("class", "vis_container")
@@ -235,33 +183,18 @@ $j( document ).ready(function () {
         .attr("class", "vis")
         .attr("width", w)
         .attr("height", h) 
-        .attr("viewBox", "0 0 " + w + " " + 10 * (h/9) ) 
-
-    
-    var clear_divs = content_containers.append("div")
-        .attr("class", "clear")
-
-    //GROUP FOR ARCS/PATHS
+        .attr("viewBox", "0 " + 0 + " " + w + " " + 5 * (h / 4) ) 
+  
+    //group for arcs/paths
     var arc_groups = vises.append("g")
         .attr("class", "arc")
-        .attr("transform", "translate(" + (w/2) + "," + (h/2) + ")");
+        .attr("transform", "translate(" + (w/2 - 40) + "," + 5 * (h / 8) + ")");
 
-    //PLACEHOLDER GRAY CIRCLE
+    //placeholder gray circle
     var loading_circles = arc_groups.append("circle")
         .attr("fill", "#000")
         .attr("r", r);
     
-    //HANDLER FOR GRAY CIRCLE
-    // $j(":not(.dropdown_container *):not(#piechart):not(#piechart *)")
-    //     .on("mouseenter", function (event) {
-    
-    //         arc_group.selectAll("circle").remove();
-    //         arc_group.append("svg:circle")
-    //             .attr("fill", "#EFEFEF")
-    //             .attr("r", r);
-
-    //     })
-
     var stat_list_containers = content_containers.append("div")
         .attr("class", "stat_list_container")
 
@@ -280,35 +213,7 @@ $j( document ).ready(function () {
     
     var label_groups = vises.append("g")
         .attr("class", "label_group")
-        .attr("transform", "translate(" + (w/2) + "," + (h/2) + ")");
-    
-    function update_data (expander) {
-        var $content = $j(expander).closest("div").next(".content")
-        if ( $content.is(":visible") ) {
-            $content.removeClass("closed") 
-            var menu = $content.children(".h_list_container").contents().get(0)
-            var cloud = d3.select(expander).data()
-            var cloud_data = JSON[cloud]
-            var data = cloud_data["stats"]
-            var menu_keys = Object.keys(data)
-            var time = cloud_data["time"]
-            
-            // update the  update time
-            $j(expander).next().text("Updated: " + time)
-            
-            var menu_items = d3.select(menu).selectAll("li")
-                .data(menu_keys)            
-    
-            menu_items.enter().append("li")
-                .text(String)
-            
-            menu_items.datum(function (d, i) { return data[menu_keys[i]] })
-          
-        }
-
-        else { $content.addClass("closed")
-             } 
-    }
+        .attr("transform", "translate(" + (w/2 - 40) + "," + 5 * (h / 8) + ")");
     
     $j(".h_list").on("click", "li", function (event) { update(event) })
 
@@ -317,27 +222,26 @@ $j( document ).ready(function () {
         setTimeout(function () { update_data(that) 
                                }, 200)
     })
-
     
     $j(".content").siblings().addBack().contents().addBack().on("mouseenter", function (event) {
         var that = event.target
-        var $vis = $j(that).hasClass("content") || $j(that).hasClass("expander") ? $j(that).closest(".title_container").siblings().addBack().find(".vis_container").contents() : $j(that).closest(".content").find(".vis_container").contents()
+        var $title = $j(that).closest(".title_container").siblings().addBack().find(".vis_container").contents() 
+        var $not_content = $j(that).closest(".content").find(".vis_container").contents()
+        var $vis = $j(that).hasClass("content") || $j(that).hasClass("expander") ? $title : $not_content 
+
         arc_group = d3.select($vis.find(".arc")[0])
         label_group = d3.select($vis.find(".label_group")[0]) 
     })
-
-
     
     ///////////////////////////////////////////////////////////
-    // MOUSEENTER HANDLER  /////////////////////////////////////
+    // FUNCTIONS //////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+    //// TWEEN FUNCTIONS TAKEN FROM D3 TUTORIAL BY ////////////
+    //// STEPHEN BOAK  ////////////////////////////////////////
     ///////////////////////////////////////////////////////////
 
-    // $j(".update").on("mouseenter", update) 
-
-    // event handler for when status attribute is moused over 
-    // to run each time data is generated
+    // handler for click on item menu in .content divs 
     function update (event) {
-        
         var that = event.target
 
         var data = d3.select(that).data()[0] 
@@ -353,9 +257,8 @@ $j( document ).ready(function () {
         } 
         
         var stat_values = [ format_stat(value) + " " + data.unit,format_stat(max_val) + " " + data.unit ]
-        
+  
         stat_list.selectAll("li").selectAll("span").filter(":last-child").remove()
-        
         stat_list.selectAll("li").append("span").text(function (d, i) { return stat_values[i] })
 
         var empty_slice = Number(max_val) - Number(value) 
@@ -366,17 +269,15 @@ $j( document ).ready(function () {
 
         pieData = pie_func(values)
         oldPieData = filteredPieData;
-               
         var totalOctets = Number(data.max_val)
 
         filteredPieData = pieData.filter(filterData);
         function filterData(element, index, array) {
             element.value = values[index];
-            return (element.value > 0);
+            return (element.value >= 0);
         }
-
                 
-        if( filteredPieData.length > 0 ){
+        if ( filteredPieData.length > 0 ) {
             arc_group.selectAll("circle").remove();
             filteredPieData = shuffle_array(filteredPieData); 
             //DRAW ARC PATHS
@@ -399,8 +300,8 @@ $j( document ).ready(function () {
                 .attrTween("d", removePieTween)
                 .remove();
 
-            var label_data = filteredPieData.filter( function (el, i) { return  !el.startAngle; })
-        
+            var label_data = filteredPieData.filter( function(el, i) { return el.value != empty_slice })
+                    
             //DRAW LABELS WITH PERCENTAGE VALUES
             valueLabels = label_group.selectAll("text.value").data(label_data)
                 .attr("dy", function(d){
@@ -417,7 +318,7 @@ $j( document ).ready(function () {
                         return "end";
                     }
                 }).text(function(d){
-                    var percentage = (d.value/totalOctets)*100;
+                    var percentage = (d.value / totalOctets) * 100;
                     return percentage.toFixed(1) + "%";
                 });
 
@@ -440,10 +341,10 @@ $j( document ).ready(function () {
           return "end";
                     }
                 }).text(function(d){
-                    var percentage = (d.value/totalOctets)*100;
+                    var percentage = (d.value / totalOctets) * 100;
                     return percentage.toFixed(1) + "%";
                 });
-            
+            // attach "capacity" label
             var text = label_group.selectAll("text.value")
             var text_bbox = text.node().getBBox()
             var text_height = text_bbox.height
@@ -459,12 +360,30 @@ $j( document ).ready(function () {
             valueLabels.exit().remove();
         }
     }
+    
+    // handler hooked on click on title that updates data stored in content 
+    function update_data (expander) {
+        var $content = $j(expander).closest("div").next(".content")
+        if ( $content.is(":visible") ) {
+            $content.removeClass("closed") 
+            var menu = $content.children(".h_list_container").contents().get(0)
+            var cloud = d3.select(expander).data()
+            var cloud_data = JSON[cloud]
+            var data = cloud_data["stats"]
+            var time = cloud_data["time"]
+            
+            // update the  update time
+            $j(expander).next().text("Updated: " + time)
+          
+            d3.select(menu).selectAll("li").datum(function (d, i) { return Object.values(data)[i] })
+          
+        }
 
-    ///////////////////////////////////////////////////////////
-    // FUNCTIONS //////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////
-
-    // Interpolate the arcs in data space.
+        else { $content.addClass("closed")
+             } 
+    }
+    
+    // interpolate the arcs in data space.
     function pieTween(d, i) {
         var s0;
         var e0;
@@ -522,11 +441,10 @@ $j( document ).ready(function () {
         };
     }
 
+    // shuffles array at random using fisher-yates  
     function shuffle_array (array) {
         var currentIndex = array.length
-        , temporaryValue
-        , randomIndex
-        ;
+        var temporaryValue, randomIndex;
         
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
@@ -544,4 +462,56 @@ $j( document ).ready(function () {
         return array;
     }
     
+
+    // main handler for expand all button
+    function toggleAll() {
+        $link.text(function() {
+            var $expanders = $j(".expander.title") 
+            var $expanded = $expanders.filter(".expanded")
+            var $collapsed = $expanders.filter(".collapsed")
+            var toggle = ""
+            if ($collapsed.length)  {
+                $link.attr("data-expander-target", "")
+                $collapsed.removeClass("collapsed").addClass("expanded")
+                toggle = "−" 
+            }
+            else {
+                $link.attr("data-expander-target", ".closed") 
+                $expanded.removeClass("expanded").addClass("collapsed")        
+                toggle = "+"
+            }
+            
+            $j(".expander.title").each( function (i, el) { 
+                setTimeout( function () { update_data(el) }, 200) 
+            });     
+            return toggle   
+        });
+    }
+
+    // following functions for expand all button animation
+
+    function expandSoon() {
+        clearTimeout(shrinkTimeout);
+        expandTimeout=setTimeout(expand,100);
+    }
+
+    function expand() {
+        clearTimeout(shrinkTimeout);
+        $heading.addClass('expand-expanded');
+        $link.attr("style","opacity:0.6;");
+    }
+
+    function shrinkSoon() {
+        clearTimeout(expandTimeout);
+        shrinkTimeout=setTimeout(shrink,100);
+    }
+
+    function shrink(){
+        clearTimeout(expandTimeout);
+        if(!$link.is(':focus') || !$link.hasClass('hover')){
+            $heading.removeClass('expand-expanded');
+            $link.attr("style", "opacity:1")
+        }        
+    }
+
 })
